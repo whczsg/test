763ce5b51293726dd934aa4bd78b56a35031f636
whczsg <1721582950@qq.com>
2025-02-10 17:25:52 +0800

    test

--- src/main/java/com/itheima/bigevent/controller/ArticleController.java
+++ src/main/java/com/itheima/bigevent/controller/ArticleController.java
@@ -15,8 +15,8 @@ import java.util.Map;
 public class ArticleController {
 
     @GetMapping("/list")
-    public Result<String> list(@RequestHeader(name = "Authorization") String token, HttpServletResponse response){
-        //验证token
+    public Result<String> list(/*@RequestHeader(name = "Authorization") String token, HttpServletResponse response*/){
+        /*//验证token
         try {
             Map<String, Object> claims = JwtUtil.parseToken(token);
             return Result.success("所有的文章数据……");
@@ -24,6 +24,8 @@ public class ArticleController {
             //http响应状态码为401
             response.setStatus(401);
             return Result.error("未登录");
-        }
+        }*/
+
+        return Result.success("所有的文章数据……");
     }
 }
